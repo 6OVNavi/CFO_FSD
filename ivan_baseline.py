75f9a61c7475a20e063bc42a92f4be968c12b736
6OVNavi <59533921+6OVNavi@users.noreply.github.com>
2022-09-23 19:31:48 +0300

    Create ivan_baseline.py

---
+++ ivan_baseline.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+
+import numpy as np
+
+import random,os
+
+seed=42
+
+def seed_everything(seed=0):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+
+seed_everything(seed)
+
+pd.set_option('display.max_columns', None)
+
+train=pd.read_parquet('train_merged.parquet')
+
+train=train.drop('DateObserve',axis=1)
+
+print(train.isna().sum())
+
+print(list(train.columns))
+
+print(train)
+
+train['CurrentPrice']=train['CurrentPrice'].fillna(0)
+
+train=pd.get_dummies(train,columns=['StockStatus'])
+
+from catboost import CatBoostRegressor
+
+X=train.drop('target',axis=1)
+y=train['target']
+
+
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+cb=CatBoostRegressor(iterations=5000,eval_metric='R2')
+cb.fit(X_train,y_train,eval_set=(X_val,y_val))
+
+print(cb.best_score_,cb.best_iteration_)
